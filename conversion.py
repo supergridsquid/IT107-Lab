bc03d9a30f45545527a747b39db593b02f56e921
gridkid87 <rnhoward51@gmail.com>
2016-10-03 03:40:02 -0600

    rewrote using boilerplate and simplified output

--- conversion.py
+++ conversion.py
@@ -1,11 +1,16 @@
-measure = input("Please type a measurement in meters: ")
-measure = float(measure)
-feet = measure * 3.281
-yards = measure * 1.094
-miles = measure * 0.0006214
-print ("The measurement in feet is:")
-print (feet)
-print ("In yards:")
-print (yards)
-print ("In miles:")
-print (miles)
+def convert(measure):
+    feet = measure * 3.281
+    yards = measure * 1.094
+    miles = measure * 0.0006214
+    
+    print ("The measurement in feet is:", feet)
+    print ("In yards:", yards)
+    print ("In miles:", miles)
+    
+    
+def main():
+    inp = float(input("Please type a measurement in meters: "))
+    convert(inp)
+    
+if __name__ == "__main__":
+    main()
