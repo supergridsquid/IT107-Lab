0f8eee7207c6013664fc2243164f52c2e9f14048
gridkid87 <rnhoward51@gmail.com>
2016-10-03 03:25:39 -0600

    lab1 files

---
+++ conversion.py
@@ -0,0 +1,11 @@
+measure = input("Please type a measurement in meters: ")
+measure = float(measure)
+feet = measure * 3.281
+yards = measure * 1.094
+miles = measure * 0.0006214
+print ("The measurement in feet is:")
+print (feet)
+print ("In yards:")
+print (yards)
+print ("In miles:")
+print (miles)
---
+++ mean.py
@@ -0,0 +1,59 @@
+v1 = input("Please enter an integer: ")
+v1 = int(v1)
+m1 = v1 + 0
+m1 = m1 / 1
+print (m1)
+
+v2 = input("Please enter an integer: ")
+v2 = int(v2)
+m2 = v1 + v2
+m2 = m2 / 2
+print (m2)
+
+v3 = input("Please enter an integer: ")
+v3 = int(v3)
+m3 = v1 + v2 + v3
+m3 = m3 / 3
+print (m3)
+
+v4 = input("Please enter an integer: ")
+v4 = int(v4)
+m4 = v1 + v2 + v3 + v4
+m4 = m4 / 4
+print (m4)
+
+v5 = input("Please enter an integer: ")
+v5 = int(v5)
+m5 = v1 + v2 + v3 + v4 + v5
+m5 = m5 / 5
+print (m5)
+
+v6 = input("Please enter an integer: ")
+v6 = int(v6)
+m6 = v1 + v2 + v3 + v4 + v5 + v6
+m6 = m6 / 6
+print (m6)
+
+v7 = input("Please enter an integer: ")
+v7 = int(v7)
+m7 = v1 + v2 + v3 + v4 + v5 + v6 + v7
+m7 = m7 / 7
+print (m7)
+
+v8 = input("Please enter an integer: ")
+v8 = int(v8)
+m8 = v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8
+m8 = m8 / 8
+print (m8)
+
+v9 = input("Please enter an integer: ")
+v9 = int(v9)
+m9 = v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9
+m9 = m9 / 9
+print (m9)
+
+v10 = input("Please enter an integer: ")
+v10 = int(v10)
+m10 = v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10
+m10 = m10 / 10
+print (m10)
